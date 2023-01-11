bc0aa0f19adb53090dfa06bd855ee24e07f025aa
Selman <mohamedsalman.siddiq@philips.com>
2023-01-11 19:24:37 +0530

    Initial commit

---
+++ app/src/main/java/com/tutorials22/broadcastreceivers/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tutorials22.broadcastreceivers.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tutorials22.broadcastreceivers.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
